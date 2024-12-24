c87cf57bd9a4e5b322b7d7b13454b3fc8c9032e9
pouriasabaghi <pouriasabaghi@gmail.com>
2024-12-24 17:56:41 +0330

    🧑‍💻 chore:  env file

--- utils/http.js
+++ utils/http.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:8000/wp-json";
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export default axios.create({
   baseURL: API_BASE_URL,
